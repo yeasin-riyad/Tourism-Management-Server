95c3dd7a7208aca424c7a69b55328a432f11f303
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-04-26 05:10:34 -0700

    Project setup done

---
+++ America.js
@@ -0,0 +1,16 @@
+const America=[{  image: "grand_canyon_image.jpg",
+tourist_spot_name: "Grand Canyon National Park",
+country_name:"United States of America",
+average_cost: {
+  entrance_fee: "$35 per vehicle (valid for 7 days)",
+  optional_activities: "Costs vary"
+},
+total_visitors_per_year: "Approximately 6.3 million visitors annually",
+travel_time: {
+  from_phoenix: "Approximately 3.5 to 4 hours by car",
+  from_las_vegas: "Approximately 4.5 to 5 hours by car"
+},
+seasonality: {
+  high_season: "March to October (Warmer months, with peak in summer)",
+  low_season: "November to February (Colder months, fewer crowds)"
+}}]
