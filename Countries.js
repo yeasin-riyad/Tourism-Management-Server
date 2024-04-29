30d57b185bc0a2e032bb770be32fd490c297bfb0
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-04-29 12:37:03 -0700

    Add some Country Name

--- Countries.js
+++ Countries.js
@@ -15,6 +15,25 @@ const countries=[
         description:"Montreal is the highest ranked and the largest city of Canada lying centrally on the floor of the island of Montreal. It has many historical tourist places in Canada with multi-cultural importance. Here you can witness some of the exotic beauty of the peak, multiple cuisines, festivals, art and culture with a French accent."
 
 
+    },
+    {
+        country_name:"Brazil",
+        image:"https://media2.thrillophilia.com/images/photos/000/372/094/original/1619197716_shutterstock_1243868206.jpg?w=753&h=450&dpr=1.0",
+        description:"Even though the city is not too large in size, it is considered to be one of the most important industrial and commercial centers of the country. On the basis of what the Ministry of Tourism, Brazil had to say, Fortaleza is found to be the “second-most desired tourist destination in Brazil.” Some of the famous beaches of the city include the bech of Futuro and the Iracema."
+    },
+    {
+        country_name:"Argentina",
+        image:"https://www.planetware.com/photos-large/ARG/argentina-top-attractions-cordoba-cathedral.jpg",
+        description:"In central Argentina, a five-hour drive from Buenos Aires, Córdoba is the country's second-largest city and is often used as a stopover on trips into the Andes. Most of the city's finest old buildings date back to the early colonial period of the 16th century."
+    },{
+        country_name:"Peru",
+        image:"https://www.planetware.com/photos-large/PER/peru-inca-trail.jpg",
+        description:"The famous Inca Trail is a four-day hike, which terminates at Machu Picchu, and is regarded by many as the highlight of their trip to Peru. This scenic trail is often more demanding than what many people are expecting, but it's also rewarding and one of the most popular things to do in Peru."
+        
+    },{
+        country_name:"Mexico",
+        image:"https://www.planetware.com/photos-large/MEX/mexico-top-places-guanajuato.jpg",
+        description:"Designated a UNESCO World Heritage Site thanks to its many old colonial buildings, winding lanes, and narrow alleys, Guanajuato is a city that just begs to be explored on foot. A particularly pleasurable experience is visiting its many plazas, including the delightful Jardin de la Union, the city's main square with its splendid old architecture. "
     }
    
 
