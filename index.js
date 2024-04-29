2d27fc1ef97bb814eacf872ceaa59cf08eaa437d
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-04-29 03:28:13 -0700

    Delete Route Done

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       const result=await cursor.toArray()
       res.send(result)
     })
+
+    app.delete('/deleteDocument/:id',async(req,res)=>{
+      const id=req.params.id
+      const query= {_id :new ObjectId(id)}
+      const result = await tourismSpots.deleteOne(query);
+      res.send(result)
+
+    })
     
     
     // Send a ping to confirm a successful connection
