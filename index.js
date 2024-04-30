1606d00ed4f94ae662749b74d5868a672b0d905b
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-04-30 05:42:12 -0700

    Bug fixed

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const tourismManagement = client.db("tourismManagement")
     const tourismSpots=tourismManagement.collection("touristSpots")
     
@@ -116,7 +116,7 @@ async function run() {
     
     
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
